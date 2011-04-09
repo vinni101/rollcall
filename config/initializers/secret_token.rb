4da3e4ccdb6428303e9f351a213f9ae80483c490
Vincent Gittens <vinni101@gmail.com>
2011-04-08 21:29:07 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rollcall::Application.config.secret_token = '6fb0786006903314888b641b4820f104771c205d1bb378f34d6a987fffae9871ca431d9ce98cc394ee2fbf16351a3f18d8aafa60f177c73e5d141e1d11b65cf7'
